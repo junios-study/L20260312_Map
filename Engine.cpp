d6be928ca25105cddf7ef79fed92f5e67740f6e2
junios-study <junios@naver.com>
2026-03-12 15:29:34 +0900

    플레이어 움직이기

--- Engine.cpp
+++ Engine.cpp
@@ -1,6 +1,10 @@
 #include "Engine.h"
 #include "conio.h"
 #include <iostream>
+#include <Windows.h>
+
+int PlayerX = 1;
+int PlayerY = 1;
 
 int Map[10][10] =
 {
@@ -18,6 +22,12 @@ int Map[10][10] =
 
 int Input()
 {
+	//if (_kbhit())
+	//{
+
+	//	_getch();
+	//}
+
 	return _getch();
 }
 
@@ -25,31 +35,68 @@ void Tick(int KeyCode)
 {
 	if (KeyCode == 'w')
 	{
-
+		AddPlayerOffset(0, -1);
 	}
 	if (KeyCode == 's')
 	{
-
+		AddPlayerOffset(0, 1);
 	}
 	if (KeyCode == 'a')
 	{
-
+		AddPlayerOffset(-1, 0);
 	}
 	if (KeyCode == 'd')
 	{
-
+		AddPlayerOffset(1, 0);
 	}
+}
+
+void Clear()
+{
+	for (int Y = 0; Y < 10; ++Y)
+	{
+		Gotoxy(1, Y);
+		std::cout << "           ";
+	}
+
+}
+
+void AddPlayerOffset(int DeltaX, int DeltaY)
+{
+	PlayerX += DeltaX;
+	PlayerY += DeltaY;
 }
 
 void Render()
 {
+	Clear();
+
 	for (int Y = 0; Y < 10; ++Y)
 	{
 		for (int X = 0; X < 10; ++X)
 		{
-			std::cout << Map[Y][X];
-		}
+			Gotoxy(X, Y);
 
-		std::cout << std::endl;
+			if (PlayerX == X && PlayerY == Y)
+			{
+				std::cout << "P";
+			}
+			else if (Map[Y][X] == 1)
+			{
+				std::cout << "#";
+			}
+			else if (Map[Y][X] == 0)
+			{
+				std::cout << " ";
+			}
+			
+		}
 	}
+}
+
+
+void Gotoxy(int x, int y)
+{
+	COORD Pos = { (SHORT)x, (SHORT)y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
 }
--- Engine.h
+++ Engine.h
@@ -5,11 +5,20 @@
 
 extern int Map[10][10];
 
+extern int PlayerX;
+extern int PlayerY;
 
 int Input();
 
 void Tick(int KeyCode);
 
 void Render();
+
+void Gotoxy(int x, int y);
+
+void Clear();
+
+void AddPlayerOffset(int DeltaX, int DeltaY);
+
 
 #endif //__ENGINE_H__
